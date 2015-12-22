0bcec989733867489b3be9e9090cfcd364f0550c
Christopher Gwizdala <cgwizdala@sapient.com>
2015-12-21 17:38:39 -0800

    moved `src` array to variable because both tasks use same files

--- example-build/grunt/config-copy.js
+++ example-build/grunt/config-copy.js
@@ -7,39 +7,28 @@ module.exports = function( grunt ) {
 
     'use strict';
 
+    var srcFiles = [
+        '**/.*',
+        '**',
+        '!.git',
+        '!.DS_Store',
+        '!docs/**',
+        '!**/less/**',
+        '!resources/**',
+        '!package.json',
+        '!Gruntfile.js',
+        '!**/readme.md',
+        '!**/css/config/**',
+        '!**/node_modules/**'
+    ];
+
     grunt.config( 'copy', {
         dev: {
-            src: [
-                '**/.*',
-                '**',
-                '!.git',
-                '!.DS_Store',
-                '!docs/**',
-                '!**/less/**',
-                '!resources/**',
-                '!package.json',
-                '!Gruntfile.js',
-                '!**/readme.md',
-                '!**/css/config/**',
-                '!**/node_modules/**'
-            ],
+            src: srcFiles,
             dest: '<%= buildPath.dev %>'
         },
         dist: {
-            src: [
-                '**/.*',
-                '**',
-                '!.git',
-                '!.DS_Store',
-                '!docs/**',
-                '!**/less/**',
-                '!resources/**',
-                '!package.json',
-                '!Gruntfile.js',
-                '!**/readme.md',
-                '!**/css/config/**',
-                '!**/node_modules/**',
-            ],
+            src: srcFiles,
             dest: '<%= buildPath.dist %>'
         }
     });
