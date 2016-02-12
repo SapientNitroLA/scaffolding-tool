989f01c0dc672ffdf1016547fdf9bed8c12408e3
Christopher Gwizdala <cgwizdala@sapient.com>
2016-02-12 14:31:06 -0800

    add mediators dynamically during build

--- example-build/grunt/config-requirejs.js
+++ example-build/grunt/config-requirejs.js
@@ -9,7 +9,8 @@ module.exports = function( grunt ) {
 
     require( 'amdefine/intercept' );
 
-    var mainConfig = require( '../library/js/config/require-config.js' )
+    var dir = require( 'node-dir' )
+        , mainConfig = require( '../library/js/config/require-config.js' )
         , defaultsDeep = require( 'lodash.defaultsdeep' )
         , common = {
             /*
@@ -40,16 +41,58 @@ module.exports = function( grunt ) {
         }
         ;
 
+    // Dynamically build module list by reading `mediators` directory.
+    // Method includes exclusion feature
+    function buildModuleList() {
+
+        dir.readFiles( 'library/js/mediators', {
+            exclude: [
+                '.DS_Store',
+                'common.js',
+                'boilerplate.js'
+            ]
+        },
+        function( err, content, filename, next ) {
+
+            if ( err ) throw err;
+            next();
+
+        },
+        function( err, files ) {
+
+            return files.map( function( file ) {
+
+                var module = {
+                    name: 'mediator/' + file.split( '/' ).pop(),
+                    exclude: mainConfig.globalExcludes
+                };
+
+                return module;
+
+            });
+
+        });
+
+    }
+
     grunt.config( 'requirejs', {
         dev: {
             options: defaultsDeep( {
                 dir: '<%= buildPath.dev %>library/',
+                modules: buildModuleList(),
+                // pragmas: {
+                //     buildExclude: true
+                // },
                 optimize: 'none' // Set 'none' so you can see how the files are concatenated.
             }, common, mainConfig )
         },
         dist: {
             options: defaultsDeep( {
                 dir: '<%= buildPath.dist %>library/',
+                modules: buildModuleList(),
+                pragmas: {
+                    buildExclude: true
+                },
                 skipDirOptimize: false
             }, common, mainConfig )
         }
