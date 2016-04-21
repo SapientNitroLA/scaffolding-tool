6cf9a8148d66b04b46d8704bfa64fc711298c9b4
Christopher Gwizdala <cgwizdala@sapient.com>
2016-04-20 19:03:00 -0700

    added postcss task

---
+++ example-build/grunt/config-postcss.js
@@ -0,0 +1,25 @@
+/*
+ * PostCSS
+ *
+ * See all options: https://github.com/nDmitry/grunt-postcss
+ */
+module.exports = function( grunt ) {
+
+    'use strict';
+
+    grunt.config( 'postcss', {
+        options: {
+            map: true, // inline sourcemaps
+            processors: [
+                require( 'autoprefixer' )( { browsers: [ 'ie 10', 'last 2 versions' ] } ) // add vendor prefixes
+            ]
+        },
+        all: {
+            expand: true,
+            src: 'library/css/**/*.css'
+        }
+    });
+
+    grunt.loadNpmTasks( 'grunt-postcss' );
+
+};
